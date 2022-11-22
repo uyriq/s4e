dc09cd74662a480e334daa1e0e9eb6ae45caec31
Uyriq <105006434+uyriq@users.noreply.github.com>
2022-11-22 18:20:13 +0300

    chore: mantine Affix для положения thoggle-teme

--- src/features/app-header.jsx
+++ src/features/app-header.jsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types'
-import { Header, MediaQuery, Burger, Text, Group, Container } from '@mantine/core'
+import { Header, Center, Flex, Burger, Text, Group, Affix, Container } from '@mantine/core'
 import ToggleThemeIcon from './toggle-theme-icon'
 import useResizeLogic from './use-resize-logic'
 
@@ -8,27 +8,40 @@ function AppHeader({ opened, setOpened }) {
 
     return (
         <Container size={reSize} px={0}>
-            <Header height={{ base: 50, md: 70 }} p="md" sx={{ display: 'flex', alignItems: 'center', height: '100%' }}>
-                <Group position="right" spacing={reSize}>
-                    <div style={{ display: 'flex', alignItems: 'center', height: '100%' }}>
-                        {' '}
-                        {(reSize === 'xs' || reSize === 'sm') && (
-                            <Burger opened={opened} onClick={() => setOpened((o) => !o)} size={reSize} mr="xl" />
-                        )}
-                        <ToggleThemeIcon size={reSize} mr="xl" />
-                    </div>
-
-                    {(reSize === 'xs' || reSize === 'sm') && (
-                        <Text hidden={!opened} position="left">
-                            {' '}
-                            Some long Application header
-                        </Text>
-                    )}
-                    {(reSize === 'md' || reSize === 'xl' || reSize === 'lg') && (
-                        <Text position="center"> Some long Application header</Text>
-                    )}
-                </Group>
-            </Header>
+            <Flex
+                direction={{ base: 'row-reverse', sm: 'row' }}
+                height={{ base: 100, xs: 50 }}
+                justify="center"
+                align="center"
+                wrap="wrap"
+            >
+                <Header height={{ base: 50, md: 70, lg: 90, xl: 120 }} p={reSize}>
+                    <Center inline={false}>
+                        <Group position="right" grow={false}>
+                            {(reSize === 'xs' || reSize === 'sm') && (
+                                <Text hidden={!opened} position="left" size={reSize}>
+                                    {' '}
+                                    Some long Application header
+                                </Text>
+                            )}
+                            {(reSize === 'md' || reSize === 'xl' || reSize === 'lg') && (
+                                <Text position="center" size={reSize}>
+                                    {' '}
+                                    Some long Application header
+                                </Text>
+                            )}
+                        </Group>
+                        <Group position="right" grow={false}>
+                            {(reSize === 'xs' || reSize === 'sm') && (
+                                <Burger opened={opened} onClick={() => setOpened((o) => !o)} size={reSize} mr="xl" />
+                            )}
+                            <Affix position={{ top: 20, right: 20 }}>
+                                <ToggleThemeIcon size={reSize} />
+                            </Affix>
+                        </Group>
+                    </Center>
+                </Header>
+            </Flex>
         </Container>
     )
 }
--- src/features/toggle-theme-icon.tsx
+++ src/features/toggle-theme-icon.tsx
@@ -12,6 +12,7 @@ export default function ToggleThemeIcon({ ...leftOver }) {
     const useStyles = createStyles((theme) => ({
         container: {
             accentColor: 'cyan',
+            alignItems: 'center',
         },
     }))
 
